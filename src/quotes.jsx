31cd0af4db2cfe55060797627a8c9970b3ff6c25
AndyTBurke <107972213+AndyTBurke@users.noreply.github.com>
2022-09-05 21:09:53 -0500

    CSS Updates & More Quotes
    title

--- src/quotes.jsx
+++ src/quotes.jsx
@@ -3,64 +3,173 @@ export default function quotes() {
     {
         id: 1,
         quotation: "An investment in knowledge pays the best interest.",
-        author: "Benjamin Franklin",
-        img: ""
+        author: "Benjamin Franklin"
     },
     {
         id: 2,
         quotation: "Bottoms in the investment world don't end with four-year lows; they end with 10- or 15-year lows.",
-        author: "Jim Rogers",
-        img: ""
+        author: "Jim Rogers"
     },
     {
         id: 3,
         quotation: "Be fearful when others are greedy. Be greedy when others are fearful.",
-        author: "Warren Buffett",
-        img: ""
+        author: "Warren Buffett"
     },
     {
         id: 4,
         quotation: "It's not whether you're right or wrong that's important, but how much money you make when you're right and how much you lose when you're wrong.",
-        author: "George Soros",
-        img: ""
+        author: "George Soros"
     },
     {
         id: 5,
         quotation: "Given a 10% chance of a 100 times payoff, you should take that bet every time.",
-        author: "Jeff Bezos",
-        img: ""
+        author: "Jeff Bezos"
     },
     {
         id: 6,
         quotation: "Don't look for the needle in the haystack. Just buy the haystack!",
-        author: "John Bogle",
-        img: ""
+        author: "John Bogle"
     },
     {
         id: 7,
         quotation: "The stock market is filled with individuals who know the price of everything, but the value of nothing.",
-        author: "Phillip Fisher",
-        img: ""
+        author: "Phillip Fisher"
     },
     {
         id: 8,
         quotation: "How many millionaires do you know who have become wealthy by investing in savings accounts?",
-        author: "Robert G. Allen",
-        img: ""
+        author: "Robert G. Allen"
     },
     {
         id: 9,
         quotation: "Courage taught me no matter how bad a crisis gets ... any sound investment will eventually pay off.",
-        author: "Carlos Slim Helu",
-        img: ""
+        author: "Carlos Slim Helu"
     },
     {
         id: 10,
         quotation: "The individual investor should act consistently as an investor and not as a speculator.",
-        author: "Ben Graham",
-        img: ""
+        author: "Ben Graham"
+    },
+    {
+        id: 11,
+        quotation: "The biggest risk of all is not taking one.",
+        author: "Mellody Hobson",
+    },
+    {
+        id: 12,
+        quotation: "Returns matter a lot.",
+        author: "Abigail Johnson",
+    },
+    {
+        id: 13,
+        quotation: "Know what you own, and know why you own it.",
+        author: "Peter Lynch",
+    },
+    {
+        id: 14,
+        quotation: "Investing should be more like watching paint dry or watching grass grow. If you want excitement, take $800 and go to Las Vegas.",
+        author: "Paul Samuelson",
+    },
+    {
+        id: 15,
+        quotation: "The four most dangerous words in investing are, it’s different this time.",
+        author: "Sir John Templeton",
+    },
+    {
+        id: 16,
+        quotation: "Invest for the long haul. Don’t get too greedy and don’t get too scared.",
+        author: "Shelby M.C. Davis",
+    },
+    {
+        id: 17,
+        quotation: "Waiting helps you as an investor and a lot of people just can’t stand to wait.",
+        author: "Charlie Munger",
+    },
+    {
+        id: 18,
+        quotation: "The stock market is a device to transfer money from the impatient to the patient.",
+        author: "Warren Buffett",
+    },
+    {
+        id: 19,
+        quotation: "The function of economic forecasting is to make astrology look respectable.",
+        author: "John Kenneth Galbraith",
+    },
+    {
+        id: 20,
+        quotation: "Far more money has been lost by investors trying to anticipate corrections, than lost in the corrections themselves.",
+        author: "Peter Lynch",
+    },
+    {
+        id: 21,
+        quotation: "Though tempting, trying to time the market is a loser’s game.",
+        author: "Christopher Davis",
+    },
+    {
+        id: 22,
+        quotation: "$10,000 continuously invested in the market over the past 20 years grew to more than $48,000. If you missed just the best 30 days, your investment was reduced to $9,900.",
+        author: "Christopher Davis",
+    },
+    {
+        id: 23,
+        quotation: "History provides a crucial insight regarding market crises: they are inevitable, painful and ultimately surmountable.",
+        author: "Shelby M.C. Davis",
+    },
+    {
+        id: 24,
+        quotation: "A 10% decline in the market is fairly common—it happens about once a year. Investors who realize this are less likely to sell in a panic.",
+        author: "Christopher Davis",
+    },
+    {
+        id: 25,
+        quotation: "In the short run, the market is a voting machine. In the long run, it is a weighing machine.",
+        author: "Benjamin Graham",
+    },
+    {
+        id: 26,
+        quotation: "A market downturn doesn't bother us. It is an opportunity to increase our ownership of great companies with great management at good prices.",
+        author: "Warren Buffett",
+    },
+    {
+        id: 27,
+        quotation: "The intelligent investor is a realist who sells to optimists and buys from pessimists.",
+        author: "Benjamin Graham",
+    },
+    {
+        id: 28,
+        quotation: "You make most of your money in a bear market, you just don't realize it at the time.",
+        author: "Shelby Cullom Davis",
+    },
+    {
+        id: 29,
+        quotation: "A lot of people with high IQs are terrible investors because they've got terrible temperaments.",
+        author: "Charlie Munger",
+    },
+    {
+        id: 30,
+        quotation: "You must value the business in order to value the stock.",
+        author: "Charlie Munger",
+    },
+    {
+        id: 31,
+        quotation: "The investor's chief problem — even his worst enemy — is likely to be himself.",
+        author: "Benjamin Graham",
+    },
+    {
+        id: 32,
+        quotation: "If you have trouble imagining a 20% loss in the stock market, you shouldn't be in stocks.",
+        author: "John Bogle",
+    },
+    {
+        id: 33,
+        quotation: "Every once in a while, the market does something so stupid it takes your breath away.",
+        author: "Jim Cramer",
+    },
+    {
+        id: 34,
+        quotation: "A stockbroker is someone who invests other people's money until it is all gone",
+        author: "Woody Allen",
     }
-
 ]
 
 return quoteData[Math.floor(Math.random() * quoteData.length)]
@@ -73,6 +182,5 @@ return quoteData[Math.floor(Math.random() * quoteData.length)]
         id: ---,
         quotation: "",
         author: "",
-        img: ""
     }
 */
